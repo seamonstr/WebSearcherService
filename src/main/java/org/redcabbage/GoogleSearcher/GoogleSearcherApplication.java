c3a1a1ab65700b8c623b03291028f6f4a541477c
Simon Woodward <simon@redcabbage.org>
2019-03-09 18:29:05 +0000

    Commit of first cut of the app.

---
+++ src/main/java/org/redcabbage/GoogleSearcher/GoogleSearcherApplication.java
@@ -0,0 +1,13 @@
+package org.redcabbage.GoogleSearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSearcherApplication.class, args);
+	}
+
+}
